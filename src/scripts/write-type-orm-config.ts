82e94b75d9795df5fe5d1578ffb427b6f5cc3150
Valdos <vladbabaev11@gmail.com>
2020-10-10 17:55:44 +0300

    migrations solution

--- src/scripts/write-type-orm-config.ts
+++
@@ -1,8 +0,0 @@
-/* eslint-disable no-magic-numbers */
-import fs = require('fs');
-import { configService } from 'src/config/config.service';
-
-fs.writeFileSync(
-  'ormconfig.json',
-  JSON.stringify(configService.getTypeOrmConfig(), null, 2),
-);
